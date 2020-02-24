7cb7dc75c023d648a7bc79d2ab0b9813c8c9ea20
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-02-24 14:10:05 -0500

    3
    3

--- CodeSegment1.cs
+++ CodeSegment1.cs
@@ -6,24 +6,39 @@ namespace LinkedList1
     {
         static void Main(String [] args)
         {
-            Fruit apple = new Fruit("Apple", 100, null); 
+            FruitBasket basket1 = new FruitBasket();
+            
+            Fruit head = new Fruit("head", 0, null, null);
+            Fruit tail = new Fruit("tail", 0, null, null);
+            Fruit apple = new Fruit("Apple", 100, head, null);
+            head.nextFruit = apple;
+            basket1.firstFruit = apple;
+            Fruit mango = new Fruit("Mango", 100, apple, null);
+            Fruit pineapple = new Fruit("pineapple", 100, mango, tail);
+            apple.nextFruit = mango;
+            mango.nextFruit = pineapple;
+            tail.previousFruit = mango;
         }
     }
     class FruitBasket
     {
-        Fruit fruit1;
+        public Fruit firstFruit;
     }
 
     class Fruit
     {
-        public Fruit(String thisFruitName, int thisCalories, Fruit aFruit)
+        public Fruit(String thisFruitName, int thisCalories, Fruit pFruit, Fruit nFruit)
         {
             fruitName = thisFruitName;
             Calories = thisCalories;
-            nextFruit = aFruit;
+            nextFruit = nFruit;
+            previousFruit = pFruit;
         }
-        String fruitName = "";
-        int Calories;
-        Fruit nextFruit;
+        public String fruitName = "";
+        public int Calories;
+        public Fruit nextFruit;
+        public Fruit previousFruit;
     }
+#to do: write a method to traverse the LL, print out the names of the fruits,
+# and sum the calories
 }
