d0ba87a4fa48928d80c335e55693133a9219bce5
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-02-24 13:31:55 -0500

    1
    1

---
+++ CodeSegment1.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace LinkedList1
+{
+    class Driver
+    {
+        static void Main(String [] args)
+        {
+
+        }
+    }
+    class FruitBasket
+    {
+        Fruit fruit1;
+    }
+
+    class Fruit
+    {
+        Fruit a;
+    }
+}
